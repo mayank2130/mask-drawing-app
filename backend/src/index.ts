39076722463cf8b4af665dde7b971ddce7db891c
mayank2130 <mayankthakur1712@gmail.com>
2024-12-14 18:17:09 +0530

    cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,11 @@ import cors from "cors"
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: "https://mask-drawing-app.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
 app.use(userRouter);
 
